d1af8a400d3132aef8942f4da58edffe2f0a2d23
fatma <fatmaalnaseebx2@gmail.com>
2025-02-11 00:29:40 +0300

    Added exercise for mutable vs immutable variables

---
+++ val_vs_var.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    var score = 10
+    println("Initial score: $score")
+    score = 20
+    println("Updated score: $score")
+    val greeting = "Hello, Kotlin!"
+    println("Greeting: $greeting")
+
+}
